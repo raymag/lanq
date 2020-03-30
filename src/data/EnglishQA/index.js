17e50c86383ba0e734089b4294f8c485af8f6beb
raymag <prayk11@outlook.com>
2020-03-30 20:44:01 -0300

    add game

---
+++ src/data/EnglishQA/index.js
@@ -0,0 +1,20 @@
+const EnglishQA = [
+    {
+        thumbnail: './assets/EnglishQA/apple.png',
+        answer: 'apple'       
+    },
+    {
+        thumbnail: './assets/EnglishQA/pencil.png',
+        answer: 'pencil'       
+    },
+    {
+        thumbnail: './assets/EnglishQA/running.png',
+        answer: 'running'       
+    },
+    {
+        thumbnail: './assets/EnglishQA/blood-drop.png',
+        answer: 'blood'       
+    },
+];
+
+export default EnglishQA;
